0d2696f191cfd6b68bc8366e4dc6e8ae30eb69e3
Lorenzo Pisani <zeelot3k@gmail.com>
2011-01-09 01:55:19 -0700

    deleting a bunch of unused classes, will recreate them later and use better versions

--- kohana/modules/yurikocms/classes/csrf.php
+++
@@ -1,6 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
-/**
- * CSRF helper class.
- */
-
-class Csrf extends Yuriko_Csrf {}
--- kohana/modules/yurikocms/classes/event.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
-
-class Event extends Yuriko_Event {}

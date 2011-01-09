6d54492099236a6c1ff2ee9c6cee80ea0984a865
Lorenzo Pisani <zeelot3k@gmail.com>
2011-01-09 01:55:57 -0700

    removing old page code

--- kohana/modules/yurikocms/classes/controller/yuriko/page.php
+++ kohana/modules/yurikocms/classes/controller/yuriko/page.php
@@ -7,62 +7,11 @@
  * @license    http://yurikocms.com/license
  */
  
-class Controller_Yuriko_Page extends Controller_Template {
+class Controller_Yuriko_Page extends Controller {
 
-	//can't auto render because template is in DB
-	public $auto_render = FALSE;
-	
-	/**
-	 * 'Builds' the page based on $permalink.
-	 * Finds all the different nodes this page is made of
-	 * and initializes all the sub-requests, placing the response
-	 * in the right section of the page.
-	 *
-	 * @param string $permalink - page permalink
-	 */
-	public function action_index($permalink = NULL)
+	public function action_view()
 	{
-		$page = ORM::factory('page')->where('permalink','=',$permalink)->find();
-
-		if ($page->loaded())
-		{
-			$this->template = new View('templates/'.$page->template);
-
-			//get all nodes on this page
-			$page_nodes = $page->page_nodes->find_all();
-			foreach ($page_nodes as $page_node)
-			{
-				$route_params = array();
-				//get this node
-				$node = $page_node->node;
-				//get custom parameters for this node
-				$params = $node->node_route_parameters->find_all();
-				foreach ($params as $param)
-				{
-					$route_params[$param->key] = $param->value;
-				}
-				//replace node params with page_node params
-				$params = $page_node->node_route_parameters->find_all();
-				foreach ($params as $param)
-				{
-					$route_params[$param->key] = $param->value;
-				}
-				//get the route
-				$node_route = $node->node_route;
-				//route name (to make the sub-request)
-				$route = route::get($node_route->name);
-				//find the uri (@TODO: parameters)
-				$uri = $route->uri($route_params);
-				//execute sub-request and put output in the right section
-				section::set($page_node->section, Request::factory($uri)
-					->execute());
-			}
-		}
-		else
-		{
-			$this->template = new View('templates/default');
-		}
-		$this->request->response = $this->template;
+		$this->response->body('Hello, World!');
 	}
 
 } // End Yuriko Page Controller
